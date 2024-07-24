67c06c720c7e533a2866921edbe6100f9cdb8a7e
fahiraaa06 <fahira1678@gmail.com>
2024-07-24 10:57:58 +0700

    update main

--- main.go
+++ main.go
@@ -9,27 +9,7 @@ import (
 )
 
 func main() {
-	// Create a new HTTP server mux
-	mux := http.NewServeMux()
-
-	// Register routes
-	mux.HandleFunc("/login", route.URL)
-	mux.HandleFunc("/refresh-token", route.URL)
-	mux.HandleFunc("/data/mahasiswa", route.URL)
-	mux.HandleFunc("/data/bimbingan/mahasiswa", route.URL)
-	mux.HandleFunc("/data/dosen", route.URL)
-	mux.HandleFunc("/jadwalmengajar", route.URL)
-	mux.HandleFunc("/riwayatmengajar", route.URL)
-	mux.HandleFunc("/absensi", route.URL)
-	mux.HandleFunc("/nilai", route.URL)
-	mux.HandleFunc("/BAP", route.URL)
-	mux.HandleFunc("/data/list/ta", route.URL)
-	mux.HandleFunc("/data/list/bimbingan", route.URL)
-	mux.HandleFunc("/approve/bimbingan", route.URL)
-
-	// Create an HTTP adapter
-	adapter := httpadapter.New(mux)
-
-	// Start the Lambda function
+	http.HandleFunc("/", route.URL)
+	adapter := httpadapter.New(http.DefaultServeMux)
 	lambda.Start(adapter.Proxy)
 }
