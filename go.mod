ccda14ab17ebd9d9bd1e597946c2ab1b52c5125b
fahiraaa06 <fahira1678@gmail.com>
2024-07-24 10:09:35 +0700

    update route url

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.21
 require (
 	github.com/PuerkitoBio/goquery v1.9.2
 	github.com/aws/aws-lambda-go v1.47.0
+	github.com/aws/aws-sdk-go-v2 v1.30.3
+	github.com/aws/aws-sdk-go-v2/config v1.27.27
+	github.com/aws/aws-sdk-go-v2/service/lambda v1.56.3
 	github.com/google/go-github/v32 v32.1.0
 	github.com/google/go-github/v59 v59.0.0
 	github.com/google/go-querystring v1.1.0
@@ -15,8 +18,21 @@ require (
 
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.3 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.27 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.3 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
--- route/route.go
+++ route/route.go
@@ -1,15 +1,49 @@
 package domyApi
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
-	config "github.com/domyid/domyapi/config"
-	controller "github.com/domyid/domyapi/controller"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	configs "github.com/domyid/domyapi/config"
 )
 
-// URL handles routing for different endpoints
+var lambdaClient *lambda.Client
+
+func init() {
+	// Initialize AWS Lambda client
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-2"))
+	if err != nil {
+		fmt.Printf("unable to load SDK config, %v", err)
+	}
+
+	lambdaClient = lambda.NewFromConfig(cfg)
+}
+
+func invokeLambda(functionName string, payload []byte) ([]byte, error) {
+	input := &lambda.InvokeInput{
+		FunctionName: aws.String(functionName),
+		Payload:      payload,
+	}
+
+	result, err := lambdaClient.Invoke(context.TODO(), input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to invoke function %s, %w", functionName, err)
+	}
+
+	if result.FunctionError != nil {
+		return nil, fmt.Errorf("function %s returned an error: %s", functionName, aws.ToString(result.FunctionError))
+	}
+
+	return result.Payload, nil
+}
+
 func URL(w http.ResponseWriter, r *http.Request) {
-	if config.SetAccessControlHeaders(w, r) {
+	if configs.SetAccessControlHeaders(w, r) {
 		return // If it's a preflight request, return early.
 	}
 
@@ -17,34 +51,59 @@ func URL(w http.ResponseWriter, r *http.Request) {
 
 	switch {
 	case method == "POST" && path == "/login":
-		controller.LoginSiakad(w, r)
+		invokeLambdaHandler(w, r, "loginLambdaFunction")
 	case method == "POST" && path == "/refresh-token":
-		controller.RefreshTokens(w, r)
+		invokeLambdaHandler(w, r, "refreshTokenLambdaFunction")
 	case method == "GET" && path == "/data/mahasiswa":
-		controller.GetMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "getMahasiswaLambdaFunction")
 	case method == "GET" && path == "/data/bimbingan/mahasiswa":
-		controller.GetListBimbinganMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "getListBimbinganMahasiswaLambdaFunction")
 	case method == "POST" && path == "/data/bimbingan/mahasiswa":
-		controller.PostBimbinganMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "postBimbinganMahasiswaLambdaFunction")
 	case method == "GET" && path == "/data/dosen":
-		controller.GetDosen(w, r)
+		invokeLambdaHandler(w, r, "getDosenLambdaFunction")
 	case method == "POST" && path == "/jadwalmengajar":
-		controller.GetJadwalMengajar(w, r)
+		invokeLambdaHandler(w, r, "getJadwalMengajarLambdaFunction")
 	case method == "POST" && path == "/riwayatmengajar":
-		controller.GetRiwayatPerkuliahan(w, r)
+		invokeLambdaHandler(w, r, "getRiwayatPerkuliahanLambdaFunction")
 	case method == "POST" && path == "/absensi":
-		controller.GetAbsensiKelas(w, r)
+		invokeLambdaHandler(w, r, "getAbsensiKelasLambdaFunction")
 	case method == "POST" && path == "/nilai":
-		controller.GetNilaiMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "getNilaiMahasiswaLambdaFunction")
 	case method == "POST" && path == "/BAP":
-		controller.GetBAP(w, r)
+		invokeLambdaHandler(w, r, "getBAPLambdaFunction")
 	case method == "GET" && path == "/data/list/ta":
-		controller.GetListTugasAkhirMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "getListTugasAkhirMahasiswaLambdaFunction")
 	case method == "POST" && path == "/data/list/bimbingan":
-		controller.GetListBimbinganMahasiswa(w, r)
+		invokeLambdaHandler(w, r, "getListBimbinganMahasiswaLambdaFunction")
 	case method == "POST" && path == "/approve/bimbingan":
-		controller.ApproveBimbingan(w, r)
+		invokeLambdaHandler(w, r, "approveBimbinganLambdaFunction")
 	default:
-		controller.NotFound(w, r)
+		http.NotFound(w, r)
 	}
+}
+
+func invokeLambdaHandler(w http.ResponseWriter, r *http.Request, functionName string) {
+	var requestBody map[string]interface{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	payload, err := json.Marshal(requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responsePayload, err := invokeLambda(functionName, payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responsePayload)
 }
