ccd01ec95a61ab3784759258f7000491c673f8af
fahiraaa06 <fahira1678@gmail.com>
2024-07-24 11:00:04 +0700

    update

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", route.URL)
+	http.HandleFunc("WebHook", route.URL)
 	adapter := httpadapter.New(http.DefaultServeMux)
 	lambda.Start(adapter.Proxy)
 }
