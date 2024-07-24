e1b2d7835569a927d4008f03b86dc02be2f7aba3
fahiraaa06 <fahira1678@gmail.com>
2024-07-24 16:07:25 +0700

    update main

--- run/main.go
+++ run/main.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	route "github.com/domyid/domyapi/route"
+	config "github.com/domyid/domyapi/config"
+	controller "github.com/domyid/domyapi/controller"
 )
 
 func main() {
-	http.HandleFunc("/", route.URL)
-	log.Println("Server is running on port 8080...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if config.SetAccessControlHeaders(w, r) {
+			return
+		}
+
+		var method, path string = r.Method, r.URL.Path
+
+		fmt.Printf("Received request - Method: %s, Path: %s\n", method, path)
+
+		switch {
+		case method == "POST" && path == "/login":
+			fmt.Println("Routing to LoginSiakad")
+			controller.LoginSiakad(w, r)
+		case method == "POST" && path == "/refresh-token":
+			fmt.Println("Routing to RefreshTokens")
+			controller.RefreshTokens(w, r)
+		case method == "GET" && path == "/data/mahasiswa":
+			fmt.Println("Routing to GetMahasiswa")
+			controller.GetMahasiswa(w, r)
+		case method == "GET" && path == "/data/bimbingan/mahasiswa":
+			fmt.Println("Routing to GetListBimbinganMahasiswa")
+			controller.GetListBimbinganMahasiswa(w, r)
+		case method == "POST" && path == "/data/bimbingan/mahasiswa":
+			fmt.Println("Routing to PostBimbinganMahasiswa")
+			controller.PostBimbinganMahasiswa(w, r)
+		case method == "GET" && path == "/data/dosen":
+			fmt.Println("Routing to GetDosen")
+			controller.GetDosen(w, r)
+		case method == "POST" && path == "/jadwalmengajar":
+			fmt.Println("Routing to GetJadwalMengajar")
+			controller.GetJadwalMengajar(w, r)
+		case method == "POST" && path == "/riwayatmengajar":
+			fmt.Println("Routing to GetRiwayatPerkuliahan")
+			controller.GetRiwayatPerkuliahan(w, r)
+		case method == "POST" && path == "/absensi":
+			fmt.Println("Routing to GetAbsensiKelas")
+			controller.GetAbsensiKelas(w, r)
+		case method == "POST" && path == "/nilai":
+			fmt.Println("Routing to GetNilaiMahasiswa")
+			controller.GetNilaiMahasiswa(w, r)
+		case method == "POST" && path == "/BAP":
+			fmt.Println("Routing to GetBAP")
+			controller.GetBAP(w, r)
+		case method == "GET" && path == "/data/list/ta":
+			fmt.Println("Routing to GetListTugasAkhirMahasiswa")
+			controller.GetListTugasAkhirMahasiswa(w, r)
+		case method == "POST" && path == "/data/list/bimbingan":
+			fmt.Println("Routing to GetListBimbinganMahasiswa")
+			controller.GetListBimbinganMahasiswa(w, r)
+		case method == "POST" && path == "/approve/bimbingan":
+			fmt.Println("Routing to ApproveBimbingan")
+			controller.ApproveBimbingan(w, r)
+		default:
+			fmt.Println("Routing to NotFound")
+			controller.NotFound(w, r)
+		}
+	})
+
+	log.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
